fcdf3d691f96faea140c0b107b760f60c80164f5
NoodlesK <mbdd0921@gmail.com>
2021-11-21 22:10:30 +0800

    Co-authored-by: Andy8787 <Andy87877@users.noreply.github.com>

--- TEST.cpp
+++ TEST.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#define zhoung "中"
+
+using namespace std;
 
 int main() {
+    system("chcp 65001");
+    cout << zhoung;
+    cout << "good morning xxther xxcker\n";
+    cout << "fami mouse\n";
     return 0;
 }
 
-// fsdofjsdfid
+/*
+hkhkkkk
+如果我把電腦重灌 是不是一切要重來:)?
+對
+QQ
+那過幾周後 我又要重來了..
+mic有時候不能收訊 可能是啥東東的問題0.0?...
+我其實不知道是什麼問題
+cool i love live owoooo
+又收不到音了QQ
+蝦?
+我在打這裡ㄟ
+哈囉030
+舒服拉
+謝謝大家
+不客氣
+ㄏㄏㄏㄏ
+oooooooojooooojjooojoojoooooojooojooojoojoooioioioiojioiooooooooooooooooooooooooooooooooooooooooooooo
+gkjlgljgl
+gkl;gkj;gj
+gk;gj
+gjklgglkgoooooooooooooooooooooooooooooooooooo
+不要誣賴我 廠管:)
+都不用段考喔030
+
+淡水
+紅樹林
+竹圍
+關渡
+忠義
+復興崗
+北投
+奇岩
+唭哩岸
+石牌
+明德
+芝山
+士林
+劍潭
+圓山
+民權西路
+雙連
+中山
+台北車站
+臺大醫院
+中正紀念堂
+東門
+大安森林公園
+大安
+信義安和
+台北101/世貿
+象山
+
+
+*/
